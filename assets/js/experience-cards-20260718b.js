6d35e9390570081c304b46b8860f65874dc2c076
Sanghyeon Ju <hiimang3@gmail.com>
2026-08-02 04:24:21 +0900

    Fix robot camp card image source

--- assets/js/experience-cards-20260718b.js
+++ assets/js/experience-cards-20260718b.js
@@ -27,7 +27,7 @@
       categories: ['extracurricular']
     },
     {
-      image: 'assets/images/experience/intelligent-robot-camp-card.jpg?v=20260802-1',
+      image: 'https://raw.githubusercontent.com/jujushmaterial/jujushmaterial.github.io/main/assets/images/experience/intelligent-robot-camp-card.jpg?v=20260802-2',
       imageClass: 'experience-card-image--camp',
       fallback: 'CAMP',
       link: 'https://jujushmaterial.github.io/2026-Intelligent-Robot-Creative-Convergence-Camp/',
